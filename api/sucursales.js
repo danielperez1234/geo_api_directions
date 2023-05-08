a2a57bb27f413fd889d6ea86ae90be4c997d3a45
dansa <dansa@DESKTOP-6M8GITO.local>
2023-05-08 16:17:37 -0600

    Actualizar sucursales.js, users.js y 2 más archivos...

--- api/sucursales.js
+++ api/sucursales.js
@@ -2,6 +2,7 @@ const SucursalController = require('../controller/sucursal');
 const express = require('express');
 const router = express.Router();
 
+router.get('/findAll',SucursalController.findAllSucursales);
 router.post('/add', SucursalController.addSucursal);
 
 module.exports = router;
