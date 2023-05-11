1cc0a1501885e19888d19d2a00fa4c9c44472c98
Manuel Peña <penanavarromanuel85@gmail.com>
2023-05-11 13:04:38 -0600

    Explicación de la API

--- models/sucursales.js
+++ models/sucursales.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const SucursalSchema = new Schema({
-    name: {type: String},
-    manager_name: {type: String},
-    latitude: {type: String},
-    longitude: {type: String},
+    name: { type: String },
+    manager_name: { type: String },
+    latitude: { type: String },
+    longitude: { type: String },
 });
+/*Exporta el modelo 'Sucursal' para que pueda ser 
+utilizado en otros archivos. Esto crea y exporta 
+un modelo de datos llamado 'Sucursal' basado en el 
+esquema definido anteriormente. El modelo se 
+registra en Mongoose utilizando el nombre 'Sucursal' 
+y se enlaza al esquema SucursalSchema.*/
 module.exports = Sucursal = mongoose.model('Sucursal', SucursalSchema);
--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require('express');
+/*Importa el modulo de "body-parser" para analizar los datos
+que se envian en el cuerpo de las solicitudes HTTP*/
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const Sucursal = require('./api/sucursales');
+//*Habilita el acceso cruzado entre dominios.
 const cors = require('cors');
 const app = express();
+//*Analiza las solicitudes con datos codificados en la URL.
 app.use(bodyParser.urlencoded({ extended: false }));
+//*Analiza las solicitudes con datos en formato JSON
 app.use(bodyParser.json());
 
-const ip = "172.18.70.193";
+const ip = "192.168.0.5";
 
+/*Monta las rutas y controladores reacionados con las 
+sucursales en la ruta base*/
 app.use('/api/sucursales', Sucursal);
+/*Permite el accesso de recursos cruzados(CORS) y agregar el encabezado
+a las respuestas*/
 app.use(
   (req, res, next) => {
     res.header("Access-Control-Allow-Origin", '*');
@@ -19,7 +28,8 @@ app.use(
 mongoose.connect(
   "mongodb://127.0.0.1:27017",
   { useNewUrlParser: true }
-).then(
+).then(/*then() se utiliza para manejar la promesa devuelta por mongoose;
+si la conexion es correcta, se inicia el servidor.*/
   () => { /** ready to use. The `mongoose.connect()` promise resolves to mongoose instance. */
     app.listen(4000, () => {
       console.log('Server running on http://' + ip + ':4000');
@@ -30,3 +40,11 @@ mongoose.connect(
     err & console.log(err) & console.log('Error connecting to db');
   }
 );
+
+/*En resumen, este código configura y ejecuta un servidor Express, 
+se conecta a una base de datos MongoDB utilizando Mongoose, 
+y establece las rutas y controladores relacionados con las sucursales.
+ Además, se habilita el middleware CORS para permitir el acceso a 
+ recursos cruzados y se configuran los middleware de análisis de 
+ solicitudes para manejar los datos enviados en el cuerpo de las 
+ solicitudes HTTP. */
